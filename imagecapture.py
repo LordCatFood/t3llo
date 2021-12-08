a9323fbddc9f2d7de339f86fff81a12cdb5b13b1
LordCatFood <39966490+LordCatFood@users.noreply.github.com>
2021-12-07 20:42:00 -0800

    Update imagecapture.py

--- imagecapture.py
+++ imagecapture.py
@@ -1,4 +1,12 @@
 import pygame
+import KeyPressModule as kp
+from time import sleep
+
+kp.init()
+
+tello = Tello()
+tello.connect()
+print(tello.get_battery())
 
 def init():
     pygame.init()
@@ -14,15 +22,32 @@ def getKey(keyName):
         ans = True
     pygame.display.update()
     return ans
+        
+def getKeyboardInput():
+    lr, fb, ud, yv = 0, 0, 0, 0
+    speed = 50
 
-def main():
-    if getKey("LEFT"):
-        print("Left key pressed")
+    if kp.getKey("LEFT"): lr = -speed
+    elif kp.getKey("RIGHT"): lr = speed
 
-    if getKey("RIGHT"):
-        print("Right key pressed")
+    if kp.getKey("UP"): fb = speed
+    elif kp.getKey("DOWN"): fb = -speed
+
+    if kp.getKey("w"): ud = speed
+    elif kp.getKey("s"): ud = -speed
+
+    if kp.getKey("a"): yv = -speed
+    elif kp.getKey("d"): yv = speed
+
+    if kp.getKey("q"): tello.land()
+    if kp.getKey("e"): tello.takeoff()
+
+    return [lr, fb, ud, yv]
 
 if __name__ == '__main__':
     init()
     while True:
-        main()
+        vals = getKeyboardInput();
+        tello.send_rc_control(vals[0], vals[1], vals[2], vals[3]);
+        getKey();
+        sleep(0.05)
